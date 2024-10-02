c0b139e7501c3f8d4c31ae3c34fc769f4b14eef3
Ashmin <ashmin2802@gmail.com>
2024-10-02 22:52:29 +0530

    complete

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -7,7 +7,7 @@ import { BehaviorSubject } from 'rxjs';
 })
 export class ApiService {
 
-  SERVER_URL="https://e-kart-server-1.onrender.com"
+  SERVER_URL="https://e-kart-server.onrender.com"
   // https://e-kart-server.onrender.com
   wishListCount=new BehaviorSubject(0)
   cartListCount=new BehaviorSubject(0)
